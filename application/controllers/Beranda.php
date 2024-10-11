547bba4f3db80089c9918fd04362d09eb9c891ee
Iqbaalhaa <iqbalhaanafi@gmail.com>
2024-10-11 18:31:37 +0700

    first commit

---
+++ application/controllers/Beranda.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Beranda extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		if ($this->session->userdata('login') == FALSE) {
+			redirect(base_url("login"));
+		}
+	}
+	public function index()
+	{
+		$data['title'] = "PMII Provinsi Jambi";
+
+		$this->load->view('header', $data);
+		$this->load->view('beranda');
+		$this->load->view('footer');
+	}
+}
---
+++ application/views/beranda.php
@@ -0,0 +1,40 @@
+<div class="content-wrapper">
+    <section class="content">
+        <div class="box box-info">
+            <div class="box-header">
+                <h4 style="text-align:center"><b>SELAMAT DATANG DI SISTEM INFORMASI PENGELOLAAN DATA <br> PERGERAKAN MAHASISWA ISLAM INDONESIA PROVINSI JAMBI</b></h4>
+                <hr>
+            </div>
+            <div style="text-align: center;">
+                <img src="<?php echo base_url('assets/images/logopmii.png'); ?>" width="400" style="margin-right:50px">
+            </div>
+            <br>
+            <br>
+        </div>
+    </section>
+</div>
+
+
+<style>
+    .main-footer {
+        background-color: #f8f9fa;
+        padding: 15px 0;
+        color: #333;
+        bottom: 0;
+        width: 100%;
+        border-top: 1px solid #dee2e6;
+    }
+
+    .main-footer a {
+        color: #007bff;
+    }
+
+    .main-footer a:hover {
+        text-decoration: underline;
+    }
+
+    .content-wrapper {
+        margin-bottom: 60px;
+        /* Adjust this value based on your footer height */
+    }
+</style>
